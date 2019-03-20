aadf7e6bb940768cf5bd00b311bef7ffc690dc8f
Nicolas Bermudez <bermnicolas@gmail.com>
2019-03-20 16:43:22 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
 
-  validate :is_title_case 
-  before_validation :make_title_case 
+  validate :is_title_case
+  before_validation :make_title_case
   belongs_to :author
 
   #put new code here
@@ -19,4 +19,5 @@ class Post < ActiveRecord::Base
   end
 
   def self.by_author(author_id)
+  end
 end
